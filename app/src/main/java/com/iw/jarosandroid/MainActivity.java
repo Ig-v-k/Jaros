b81abe9232b71d911555706fcb6962e5c2a7233f
Igor Wysocki <lgorvysocky@gmail.com>
2023-07-04 00:34:12 +0300

    home ui

--- app/src/main/java/com/iw/jarosandroid/MainActivity.java
+++ app/src/main/java/com/iw/jarosandroid/MainActivity.java
@@ -3,13 +3,16 @@ package com.iw.jarosandroid;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.iw.jarosandroid.facet.HomeFacet;
 import com.iw.jarosandroid.route.ContainerRoute;
 
 public final class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final Route route = new ContainerRoute(getSupportFragmentManager());
         setContentView(R.layout.a_main);
+
+        new ContainerRoute(getSupportFragmentManager()).replace(new HomeFacet());
     }
 }
---
+++ app/src/main/java/com/iw/jarosandroid/facet/HomeFacet.java
@@ -0,0 +1,62 @@
+package com.iw.jarosandroid.facet;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.iw.jarosandroid.Facet;
+import com.iw.jarosandroid.R;
+import org.jetbrains.annotations.NotNull;
+
+public final class HomeFacet extends Fragment implements Facet {
+
+    private Context context;
+    private View vMain;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        vMain = inflater.inflate(R.layout.f_home, container, false);
+
+        return vMain;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view,
+                              @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public String tag() {
+        return "HomeFacet";
+    }
+
+    @Override
+    public String name() {
+        return "";
+    }
+
+    @Override
+    public Fragment fragment() {
+        return this;
+    }
+
+    @Override
+    public Facet self() {
+        return new HomeFacet();
+    }
+}
