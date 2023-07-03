1174bb5f2367f29a56a4d3a78ce3ebfa105c0ad8
Igor Wysocki <lgorvysocky@gmail.com>
2023-07-03 20:32:52 +0300

    container route + container fit system window

--- app/src/main/java/com/iw/jarosandroid/MainActivity.java
+++ app/src/main/java/com/iw/jarosandroid/MainActivity.java
@@ -3,37 +3,13 @@ package com.iw.jarosandroid;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
+import com.iw.jarosandroid.route.ContainerRoute;
 
-public final class MainActivity extends AppCompatActivity implements Route {
-
+public final class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Route route = new ContainerRoute(getSupportFragmentManager());
         setContentView(R.layout.a_main);
-    }
-
-    @Override
-    public void forward(final Facet facet) {
-        getSupportFragmentManager()
-                .beginTransaction()
-                .setReorderingAllowed(true)
-                .add(R.id.v_main_container, facet.fragment(), facet.tag())
-                .commit();
-    }
-
-    @Override
-    public void back() {
-        getSupportFragmentManager().popBackStack();
-    }
-
-    @Override
-    public void replace(final Facet facet) {
-        final FragmentManager fManager = getSupportFragmentManager();
-        fManager.popBackStack();
-        fManager.beginTransaction()
-                .setReorderingAllowed(false)
-                .replace(R.id.v_main_container, facet.fragment(), facet.tag())
-                .commit();
     }
 }
---
+++ app/src/main/java/com/iw/jarosandroid/route/ContainerRoute.java
@@ -0,0 +1,36 @@
+package com.iw.jarosandroid.route;
+
+import androidx.fragment.app.FragmentManager;
+import com.iw.jarosandroid.Facet;
+import com.iw.jarosandroid.R;
+import com.iw.jarosandroid.Route;
+
+public final class ContainerRoute implements Route {
+    private final FragmentManager manager;
+
+    public ContainerRoute(FragmentManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public void forward(Facet facet) {
+        manager.beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.v_main_container, facet.fragment(), facet.tag())
+                .commit();
+    }
+
+    @Override
+    public void back() {
+        manager.popBackStack();
+    }
+
+    @Override
+    public void replace(Facet facet) {
+        manager.popBackStack();
+        manager.beginTransaction()
+                .setReorderingAllowed(false)
+                .replace(R.id.v_main_container, facet.fragment(), facet.tag())
+                .commit();
+    }
+}
