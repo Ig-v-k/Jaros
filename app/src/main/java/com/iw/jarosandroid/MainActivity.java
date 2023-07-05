41f28f3a31fa4d4c66162c37b1ed1552b325d539
Igor Wysocki <lgorvysocky@gmail.com>
2023-07-05 21:13:06 +0300

    local data

--- app/src/main/java/com/iw/jarosandroid/MainActivity.java
+++ app/src/main/java/com/iw/jarosandroid/MainActivity.java
@@ -18,6 +18,7 @@ import com.google.android.material.search.SearchBar;
 import com.google.android.material.search.SearchView;
 import com.iw.jarosandroid.facet.HomeFacet;
 import com.iw.jarosandroid.facet.ProductFacet;
+import com.iw.jarosandroid.product.SimpleProduct;
 import com.iw.jarosandroid.route.ContainerRoute;
 
 import java.util.ArrayList;
@@ -28,18 +29,22 @@ public final class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_main);
 
-        final ArrayList<String> data = new ArrayList<String>() {{
-            add("Rolada schabowa");
-            add("Mielony wieprzowy");
-            add("Golabek");
-            add("Pieczen z karkowki");
-            add("Kotlet schabowy");
-            add("Filet z drobiu");
-            add("Roztrzepaniec");
-            add("Rolada schabowa");
+        final ArrayList<Product> products = new ArrayList<Product>() {{
+            add(new SimpleProduct(1, "Rolada schabowa", "Mieso", "Meal", "14", "product1"));
+            add(new SimpleProduct(2, "Mielony wieprzowy", "Mieso", "Meal", "14", ""));
+            add(new SimpleProduct(3, "Golabek", "Mieso", "Meal", "14", ""));
+            add(new SimpleProduct(4, "Pieczen z karkowki", "Mieso", "Meal", "14", ""));
+            add(new SimpleProduct(5, "Kotlet schabowy", "Mieso", "Meal", "14", ""));
+            add(new SimpleProduct(6, "Filet z drobiu", "Mieso", "Meal", "14", ""));
+            add(new SimpleProduct(7, "Roztrzepaniec", "Mieso", "Meal", "14", ""));
+            add(new SimpleProduct(8, "Schab po meksykansku", "Mieso", "Meal", "14", ""));
+            add(new SimpleProduct(9, "Kotlet zapiekany", "Mieso", "Meal", "14", ""));
+            add(new SimpleProduct(10, "Devolay", "Mieso", "Meal", "14", ""));
+            add(new SimpleProduct(11, "Potrawka z kurczaka", "Mieso", "Meal", "14", ""));
+            add(new SimpleProduct(12, "Kotlet pozarski", "Mieso", "Meal", "14", ""));
         }};
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, products);
 
         final ListView vList = new ListView(MainActivity.this);
         vList.setAdapter(adapter);
